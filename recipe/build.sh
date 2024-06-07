9a02ca6f001821b81c284e84bf90a454fca42f52
Sebastian Ehlert <sehlert@microsoft.com>
2024-06-07 19:09:13 +0200

    Add all arguments from MESON_ARGS for meson-python

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,11 +7,14 @@ cat > ${SRC_DIR}/pkgconfig.ini <<EOF
 pkgconfig = '$BUILD_PREFIX/bin/pkg-config'
 EOF
 
+IFS=' ' read -r -a meson_args <<< "${MESON_ARGS}"
+
 mv python/mesonpep517.toml python/pyproject.toml
 
 ${PYTHON} -m build \
    -Csetup-args="--warnlevel=0" \
    -Csetup-args="--cross-file=${SRC_DIR}/pkgconfig.ini" \
+   ${meson_args[@]/#/-Csetup-args=} \
    --outdir . \
    --no-isolation \
    --wheel \
