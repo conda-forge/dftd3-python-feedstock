700d5cbdbb6c4b4182151daf9b081bf520b575a2
Sebastian Ehlert <sehlert@microsoft.com>
2024-06-08 11:04:17 +0200

    Use meson for cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,13 +15,26 @@ if [ -f ${BUILD_PREFIX}/meson_cross_file.txt ]; then
   build_args=("${build_args[@]}" "-Csetup-args=--cross-file=${BUILD_PREFIX}/meson_cross_file.txt")
 fi
 
-mv python/mesonpep517.toml python/pyproject.toml
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+  mv python/mesonpep517.toml python/pyproject.toml
 
-${PYTHON} -m build \
-   "${build_args[@]}" \
-   --outdir . \
-   --no-isolation \
-   --wheel \
-   python/
+  ${PYTHON} -m build \
+    "${build_args[@]}" \
+    --outdir . \
+    --no-isolation \
+    --wheel \
+    python/
 
-${PYTHON} -m pip install *.whl --no-deps -vv
+  ${PYTHON} -m pip install *.whl --no-deps -vv
+else
+  meson setup \
+    ${MESON_ARGS} \
+    --warnlevel=0 \
+    --cross-file=${SRC_DIR}/pkgconfig.ini \
+    _build \
+    python/
+
+  meson compile -C _build
+  meson install -C _build
+fi
+
