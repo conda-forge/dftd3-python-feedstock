9e33f904f1f65a5d1d9f72169827d024e70faace
Sebastian Ehlert <sehlert@microsoft.com>
2024-06-08 12:50:07 +0200

    Explicitly point to Python version in PREFIX

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,31 +10,19 @@ EOF
 build_args=(
   "-Csetup-args=--warnlevel=0"
   "-Csetup-args=--cross-file=${SRC_DIR}/pkgconfig.ini"
+  "-Csetup-args=-Dpython_version=${PREFIX}/bin/python3"
 )
 if [ -f ${BUILD_PREFIX}/meson_cross_file.txt ]; then
   build_args=("${build_args[@]}" "-Csetup-args=--cross-file=${BUILD_PREFIX}/meson_cross_file.txt")
 fi
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  mv python/mesonpep517.toml python/pyproject.toml
+mv python/mesonpep517.toml python/pyproject.toml
 
-  ${PYTHON} -m build \
+${PYTHON} -m build \
     "${build_args[@]}" \
     --outdir . \
     --no-isolation \
     --wheel \
     python/
 
-  ${PYTHON} -m pip install *.whl --no-deps -vv
-else
-  meson setup \
-    ${MESON_ARGS} \
-    --warnlevel=0 \
-    --cross-file=${SRC_DIR}/pkgconfig.ini \
-    _build \
-    python/
-
-  meson compile -C _build
-  meson install -C _build
-fi
-
+${PYTHON} -m pip install *.whl --no-deps -vv
